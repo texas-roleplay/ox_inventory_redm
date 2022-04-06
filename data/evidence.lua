aece848d9bf5b274018c12df11a815a0d2c9c289
klandestino7 <klandestino6@hotmail.com>
2022-04-06 19:23:04 -0300

    feat[stash]: correção no bau de evidencias da polícia

--- data/evidence.lua
+++ data/evidence.lua
@@ -15,9 +15,8 @@ return {
 
 	{	name = "evidence_saint_dennis",	coords = vec3(2903.885, 1309.876, 44.937) }, -- annesburg
 	{	name = "evidence_rhodes",	coords = vec3(1362.08, -1301.896, 77.768) },  -- rhodes
-	{	name = "evidence_valentine",	coords = vec3(-287.2235, 803.3991, 119.38) }, -- valentine
+	{	name = "evidence_valentine",	coords = vec3(-278.2352, 803.5825, 119.40) }, -- valentine
 	{	name = "evidence_strawberry",	coords = vec3(-1812.369, -355.767, 164.649) }, -- strawberry
 	{	name = "evidence_blackwater",	coords = vec3(-768.045, -1266.589, 44.053) }, -- blackwater
 	{	name = "evidence_thumbweed",	coords = vec3(-5531.527, -2929.234, -1.3610) }, -- thumbweed
-
 }
